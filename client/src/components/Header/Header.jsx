f539613c628f3df7367bd0207be503028c20ea59
ArtyomHloba <artemgloba2@gmail.com>
2024-11-20 13:21:43 +0200

    Refactor names for sections

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -126,7 +126,7 @@ class Header extends React.Component {
             <div className={styles.nav}>
               <ul>
                 <li>
-                  <span>NAME IDEAS</span>
+                  <span>Name Ideas</span>
                   <img
                     src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
                     alt='menu'
@@ -159,32 +159,32 @@ class Header extends React.Component {
                   </ul>
                 </li>
                 <li>
-                  <span>CONTESTS</span>
+                  <span>Contests</span>
                   <img
                     src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
                     alt='menu'
                   />
                   <ul>
                     <li>
-                      <Link to='/howItWorks'>HOW IT WORKS</Link>
+                      <Link to='/howItWorks'>How It Works</Link>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>PRICING</a>
+                      <a href='http://www.google.com'>Pricing</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>AGENCY SERVICE</a>
+                      <a href='http://www.google.com'>Agency Servise</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>ACTIVE CONTESTS</a>
+                      <a href='http://www.google.com'>Active Contests</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>WINNERS</a>
+                      <a href='http://www.google.com'>Winners</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>LEADERBOARD</a>
+                      <a href='http://www.google.com'>Leaderboard</a>
                     </li>
                     <li className={styles.last}>
-                      <a href='http://www.google.com'>BECOME A CREATIVE</a>
+                      <a href='http://www.google.com'>Become A Creative</a>
                     </li>
                   </ul>
                 </li>
@@ -196,16 +196,16 @@ class Header extends React.Component {
                   />
                   <ul>
                     <li>
-                      <a href='http://www.google.com'>NAMES</a>
+                      <a href='http://www.google.com'>Names</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>TAGLINES</a>
+                      <a href='http://www.google.com'>Taglines</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>LOGOS</a>
+                      <a href='http://www.google.com'>Logos</a>
                     </li>
                     <li className={styles.last}>
-                      <a href='http://www.google.com'>TESTIMONIALS</a>
+                      <a href='http://www.google.com'>Testimonials</a>
                     </li>
                   </ul>
                 </li>
@@ -217,22 +217,22 @@ class Header extends React.Component {
                   />
                   <ul>
                     <li>
-                      <a href='http://www.google.com'>POPULAR NAMES</a>
+                      <a href='http://www.google.com'>Popular Names</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>SHORT NAMES</a>
+                      <a href='http://www.google.com'>Short Names</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>INTRIGUING NAMES</a>
+                      <a href='http://www.google.com'>Intriguing Names</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>NAMES BY CATEGORY</a>
+                      <a href='http://www.google.com'>Names By Category</a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>VISUAL NAME SEARCH</a>
+                      <a href='http://www.google.com'>Visual Name Search</a>
                     </li>
                     <li className={styles.last}>
-                      <a href='http://www.google.com'>SELL YOUR DOMAINS</a>
+                      <a href='http://www.google.com'>Sell Your Domains</a>
                     </li>
                   </ul>
                 </li>
@@ -244,18 +244,18 @@ class Header extends React.Component {
                   />
                   <ul>
                     <li>
-                      <a href='http://www.google.com'>ULTIMATE NAMING GUIDE</a>
+                      <a href='http://www.google.com'>Ultimate Naming Guide</a>
                     </li>
                     <li>
                       <a href='http://www.google.com'>
-                        POETIC DEVICES IN BUSINESS NAMING
+                        Poetic Devices In Business Naming
                       </a>
                     </li>
                     <li>
-                      <a href='http://www.google.com'>CROWDED BAR THEORY</a>
+                      <a href='http://www.google.com'>Crowded Bar Theory</a>
                     </li>
                     <li className={styles.last}>
-                      <a href='http://www.google.com'>ALL ARTICLES</a>
+                      <a href='http://www.google.com'>All Articles</a>
                     </li>
                   </ul>
                 </li>
