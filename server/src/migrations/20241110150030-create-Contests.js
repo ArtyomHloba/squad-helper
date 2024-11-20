d5f15fa7e93f5cf6031121328f746c05c1578760
ArtyomHloba <artemgloba2@gmail.com>
2024-11-20 12:34:05 +0200

    Fix migrations for Contests

--- server/src/migrations/20241110150030-create-Contests.js
+++ server/src/migrations/20241110150030-create-Contests.js
@@ -1,48 +1,92 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Offers', {
+    return queryInterface.createTable('Contests', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      userId: {
-        type: Sequelize.INTEGER,
+      contestType: {
         allowNull: false,
+        type: Sequelize.ENUM('name', 'tagline', 'logo'),
+      },
+      fileName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      originalFileName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      title: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      typeOfName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      industry: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      focusOfWork: {
+        allowNull: true,
+        type: Sequelize.TEXT,
+      },
+      targetCustomer: {
+        allowNull: true,
+        type: Sequelize.TEXT,
+      },
+      styleName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      nameVenture: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      typeOfTagline: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      brandStyle: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      prize: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      priority: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      orderId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
         references: {
           model: 'Users',
           key: 'id',
         },
       },
-      contestId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Contests',
-          key: 'id',
-        },
-      },
-      text: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      fileName: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      originalFileName: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      status: {
-        type: Sequelize.STRING,
-        allowNull: true,
-        defaultValue: 'pending',
-      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Offers');
+    return queryInterface.dropTable('Contests');
   },
 };
