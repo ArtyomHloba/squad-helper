255999c250ab88600d0e8bd629f0fdc0e0eebb2a
ArtyomHloba <artemgloba2@gmail.com>
2024-11-29 12:24:41 +0200

    Add buttonOptions file

---
+++ client/src/pages/ContestCreation/ButtonGroup/buttonOptions.js
@@ -0,0 +1,22 @@
+const buttonOptions = [
+  {
+    id: 1,
+    label: 'Yes',
+    description: 'But minor variations are allowed',
+    recommended: true,
+  },
+  {
+    id: 2,
+    label: 'Yes',
+    description: 'The Domain should exactly match the name',
+    recommended: false,
+  },
+  {
+    id: 3,
+    label: 'No',
+    description: 'I am only looking for a name, not a Domain',
+    recommended: false,
+  },
+];
+
+export default buttonOptions;
